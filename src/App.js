95a3278a9f238e6b6491af0e2ba6c729f8ccd516
Chandra Prakash Bathula <chandraprakashbathula@Mac.attlocal.net>
2025-07-05 00:27:54 -0500

    hiding navbar in login page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Link,
+  useLocation
 } from "react-router-dom";
 
 import Login from "./pages/Login";
@@ -11,23 +12,33 @@ import Regions from "./pages/Regions";
 import Video from "./pages/Video";
 
 function App() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/";
+
   return (
-    <Router>
-      <nav className="flex gap-4 p-4 bg-gray-200">
-        <Link to="/">Login</Link>
-        <Link to="/home">Home</Link>
-        <Link to="/regions">Regions</Link>
-        <Link to="/video">Video</Link>
-      </nav>
+    <>
+      {!hideNavbar && (
+        <nav className="flex gap-4 p-4 bg-gray-200">
+          <Link to="/home" className="cursor-pointer">Home</Link>
+          <Link to="/regions" className="cursor-pointer">Regions</Link>
+          <Link to="/video" className="cursor-pointer">Video</Link>
+        </nav>
+      )}
 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/regions" element={<Regions />} />
-         <Route path="/video" element={<Video />} />
+        <Route path="/video" element={<Video />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
